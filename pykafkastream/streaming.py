3a35bf05f64005a94503ba0d65d4ff6acc6907e5
Mohammed Mujahed Khan <mujahed08@hotmail.com>
2022-05-21 18:55:16 +0530

    bcp

--- pykafkastream/streaming.py
+++ pykafkastream/streaming.py
@@ -4,8 +4,8 @@ from pyspark.sql.streaming import *
 spark = SparkSession \
     .builder \
     .appName("Spark Kafka Streaming") \
-    .master("spark://sparkd:7077") \
-    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0") \
+    .master("spark://192.168.1.39:7077") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3") \
     .getOrCreate()
 
 # spark.sparkContext.setLogLevel("ERROR")
@@ -13,7 +13,7 @@ spark = SparkSession \
 df = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafkad:9092") \
+    .option("kafka.bootstrap.servers", "localhost:9093") \
     .option("subscribe", "T.LEARNER_FILES.EVENT") \
     .option("startingOffsets", "earliest") \
     .load()
